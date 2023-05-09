18465dce6493503d9684c2268b9700bc2cc9125d
Habte Selassie <habtuchelsea1000@gmail.com>
2023-05-09 18:20:25 +0300

    update

--- front/src/app/user/list-user/list-user.component.ts
+++ front/src/app/user/list-user/list-user.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-user.component.css']
 })
 export class ListUserComponent {
+  displayedColumns: string[] = ['position', 'name', 'age', 'email', 'department', 'designation', 'skills', 'experience', 'salary', 'contact', 'address','actions'];
 
+  dataSource: any[] = [
+    { position: 1, name: 'John Doe', age: 30, email: 'johndoe@example.com', department: 'IT', designation: 'Software Engineer', skills: 'Angular, TypeScript', experience: '5 years', salary: '$100,000', contact: '123-456-7890', address: '123 ABC Street' },
+    // Add more data objects as needed
+  ]3
 }
